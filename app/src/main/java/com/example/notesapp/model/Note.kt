5d27f6a2ceea8ab7034ced8d0954bcf8be94331a
AVtheking <lyfflame45@gmail.com>
2023-06-20 17:27:14 +0530

    entities and dao for database is added

--- app/src/main/java/com/example/notesapp/model/Note.kt
+++ app/src/main/java/com/example/notesapp/model/Note.kt
@@ -1,3 +1,17 @@
 package com.example.notesapp.model
 
-data class Note()
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val notetitle:String,
+    val notebody:String
+):Parcelable
+
+
